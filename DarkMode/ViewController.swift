40d86f51cf91862274e73db5aede8f167e838dd7
sumeyrademirtas <smyrdmrts@gmail.com>
2024-06-10 13:37:10 +0300

    feat: Implement dynamic button tint color based on user interface style
    - Set view controller's user interface style to light mode overriding user's system preference.
    - Added traitCollectionDidChange method to handle changes in user interface style.
    - Changed button tint color to red when in dark mode and blue when in light mode.

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       overrideUserInterfaceStyle = .light // view controller a ozel
+        //kullanicinin telefonunda su an hangi mode u kullandigi onemli olmadan light a aliyor
+        //eger butun uygulamaya ozel olsun istersek, info plist e gidip User Interface Style dan belirleyebiliriz
+        
+
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.red
+            
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
     }
 
 
